751494f5d51941fdabd4691917e4197d7267bbb0
James DeLay <jamesdelay@pop-os.localdomain>
2019-09-11 22:08:55 -0400

    styled fonts on navbar

--- r-books/src/components/Navigation.vue
+++ r-books/src/components/Navigation.vue
@@ -1,13 +1,13 @@
 <template>
     <nav class="nav-bar">
         <div class="logo">
-            Books
+            <p class="text">r / books</p>
         </div>
         <div class="links">
-            <a>About</a>
-            <a>Explore</a>
-            <a>Contact</a>
-            <a>FAQ</a>
+            <a class="text">About</a>
+            <a class="text">Explore</a>
+            <a class="text">Contact</a>
+            <a class="text">FAQ</a>
         </div>
     </nav>
 </template>
@@ -19,6 +19,8 @@
 </script>
 
 <style lang="scss">
+    @import url('https://fonts.googleapis.com/css?family=Modak&display=swap');
+
     .nav-bar {
         position: fixed;
         z-index: 10;
@@ -27,20 +29,47 @@
         display: flex;
         background-image: linear-gradient(to bottom, rgba(52, 52, 52, 1), rgb(44, 44, 44));
     }
+
     .logo {
-        width: 60%;
+        width: 40%;
+        display: flex;
+        align-items: center;
+        padding-left: 2rem;
+
+        p {
+            color: rgba(255, 255, 250, 1);
+            font-size: 3rem;
+            font-family: "Modak";
+            -webkit-text-stroke: 2px rgba(140, 39, 30, 1);;
+        }
+        .text:hover {
+            -webkit-text-stroke: 3px salmon;
+        }
     }
+    
+
     .links {
         display: flex;
         justify-content: space-around;
         align-items: center;
-        width: 40%;
-        cursor: pointer;
+        width: 60%;
         a {
-            // color: 
             font-size: 1.5rem;
-            text-shadow: 1px 1px rgba(52, 52, 52, 1);
-            color: rgba(255, 230, 109, 1);
+            color: rgba(255, 255, 250, 1);
+            font-family: "Modak";
+            -webkit-text-stroke: 1px rgba(140, 39, 30, 1);;
         }
+    }
+
+    .text {
+        transition: .5s;
+        cursor: pointer;  
+    }
+
+    .text:hover {
+        color: rgba(255, 255, 250, 1);
+        -webkit-text-stroke: 2px salmon;
+        transform: scale(1.25);
+        z-index: 1;
     }
 </style>
